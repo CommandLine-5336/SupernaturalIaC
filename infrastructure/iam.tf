0745261f3e9f1fba9932bec1ffd2b35ae5d1daec
Derankatinum <50229852+Derankatinum@users.noreply.github.com>
2026-08-06 12:17:06 +0300

    Merge pull request #2 from CommandLine-5336/chore/CL-67-create-iam-roles
    chore/CL-67-create-IAM-roles

---
+++ infrastructure/iam.tf
@@ -0,0 +1,24 @@
+module "eks_cluster_role" {
+  source           = "../modules/iam_for_eks"
+  name             = "eks-cluster-role"
+  trusted_services = ["eks.amazonaws.com"]
+
+  aws_managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSBlockStoragePolicyV2",
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSComputePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSNetworkingPolicy",
+  ]
+}
+module "eks_node_role" {
+  source           = "../modules/iam_for_eks"
+  name             = "eks-node-role"
+  trusted_services = ["ec2.amazonaws.com"]
+
+  aws_managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly",
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodeMinimalPolicy",
+    "arn:aws:iam::aws:policy/AmazonElasticContainerRegistryPublicReadOnly",
+  ]
+}
