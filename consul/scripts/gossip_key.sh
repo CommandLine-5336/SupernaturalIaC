34dba7db612bba9cdb49db8a01480ab7228389f2
BozhenaOliinyk <bozhenaolijnyk01@gmail.com>
2026-08-08 23:31:13 +0300

    feature: CL-58 deleted script and added README

--- consul/scripts/gossip_key.sh
+++
@@ -1,7 +0,0 @@
-# Generate key
-openssl rand -base64 32
-
-# Create Kubernetes secret
-kubectl create secret generic consul-gossip-encryption-key \
-  --namespace consul \
-  --from-literal=key="<generated-key>"
